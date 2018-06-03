3bd25fc8f52bbd8bd5f2824b3f5da6608d549f88
Ross Edelman <ross.edelman123@gmail.com>
2018-06-03 13:17:50 -0400

    Text input added.

--- rn-course/App.js
+++ rn-course/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    placeName: ""
+  };
+
+  //Must be arrow function so this refers to class. 
+  placeNameChangedHandler = val => {
+    //This refers to class.
+    this.setState({
+      placeName:val
+    });
+
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput 
+          style={{width: 300, borderColor: "black",borderWidth:1}}
+          value={this.state.placeName} 
+          onChangeText={this.placeNameChangedHandler}
+        />
       </View>
     );
   }
