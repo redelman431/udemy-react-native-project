e8499efbf83d246a0600076a341116fd9f2c0fbf
Ross Edelman <ross.edelman123@gmail.com>
2018-06-03 17:13:03 -0400

    Styling and centering of button and textarea.

--- rn-course/App.js
+++ rn-course/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
   state = {
@@ -18,12 +18,15 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
-          style={{width: 300}}
-          placeholder="An awesome place"
-          value={this.state.placeName} 
-          onChangeText={this.placeNameChangedHandler}
-        />
+       <View style={styles.inputContainer}> 
+         <TextInput 
+            placeholder="An awesome place"
+            value={this.state.placeName} 
+            onChangeText={this.placeNameChangedHandler}
+            style={styles.placeInput}          
+          />
+          <Button title="Add" style={styles.placeButton} />
+        </View>
       </View>
     );
   }
@@ -37,4 +40,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start'
   },
+  inputContainer:{
+    width:"100%",
+    flexDirection: "row",
+    justifyContent:"space-between",
+    alignItems:"center"
+  },
+  placeInput: {
+    width:"70%"
+  },
+  placeButton: {
+    width:"30%"
+
+  }
 });
