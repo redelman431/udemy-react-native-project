4bf240ed611443d04f7ea816685f088734904211
Ross Edelman <ross@localhost.localdomain>
2018-06-07 17:45:30 -0400

    Modification according to instructor's solution.

--- rn-course/App.js
+++ rn-course/App.js
@@ -1,42 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-
-import ListItem from './src/components/ListItem/ListItem';
-import TextSubmit from './src/components/TextSubmit/TextSubmit';
+import { StyleSheet, View } from 'react-native';
+import PlaceInput from './src/components/PlaceInput/PlaceInput';
+import PlaceList from './src/components/PlaceList/PlaceList';
 
 export default class App extends React.Component {
   state = {
     places: []
   };
-  
-  //Allows state change of places from another component. 
-  changePlaces = (newPlace) => {
-    
-    this.setState(prevState => {
-      return {
-       places:prevState.places.concat(newPlace)
-      };
-     
-    })
-  };
 
-  render() {
-    //Iterates text jsx element into array for output in view.
-    const placesOutput = this.state.places.map((place,i) => (
-      <ListItem key={i} placeName={place} /> 
-      
-    ));
-    return (
-      <View style={styles.container}>
-        <TextSubmit
-          onPlaceSubmitHandler={this.state.places}
-          changePlaces={this.changePlaces.bind(this)}
-        
-        />
-        <View style={styles.listContainer}>
-          {placesOutput}
-        </View>
-      </View>
+  placeAddedHandler = placeName => {
+
+    this.setState(prevState => {
+        return {
+            places: prevState.places.concat(placeName)
+        };
+    });
+    
+};
+
+render() {
+  return (
+    <View style={styles.container}>
+      <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+      <PlaceList places ={this.state.places} />   
+    </View>
     );
   }
 }
@@ -48,8 +35,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start'
-  },
-  listContainer: {
-    width:"100%"
   }
 });
