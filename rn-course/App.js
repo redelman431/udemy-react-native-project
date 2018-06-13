353798442fe79e1d798e43afc601e3ffb0604ef9
Ross Edelman <ross@localhost.localdomain>
2018-06-13 17:21:04 -0400

    Authentication login button.

--- rn-course/App.js
+++ rn-course/App.js
@@ -1,14 +1,18 @@
 import { Navigation } from 'react-native-navigation';
+
 import AuthScreen from './src/screens/Auth/Auth';
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
-
-//Register screens.
+//Register screens 
 Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen)
+Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen)
 
 //Start React Navigation in app. 
 Navigation.startSingleScreenApp({
   screen:{
     screen:"awesome-places.AuthScreen",
-    title:"Login"
+    title:"Login!"
   }
 });
