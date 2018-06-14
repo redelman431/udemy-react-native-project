a60a2f429adea7d74f47ee8f8e2e7c570c15e917
Ross Edelman <ross@localhost.localdomain>
2018-06-14 16:05:36 -0400

    Find place and share place icon tabs.

--- rn-course/src/screens/MainTabs/startMainTabs.js
+++ rn-course/src/screens/MainTabs/startMainTabs.js
@@ -1,21 +1,31 @@
 import {Navigation} from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-    Navigation.startTabBasedApp({
-        tabs: [
-            {
-                screen: "awesome-places.FindPlaceScreen",
-                label:  "Find Place",
-                title: "Find Place"
-            },
-            {
-                screen: "awesome-places.SharePlaceScreen",
-                label:  "Share Place",
-                title: "Share Place"
-            }
-        ]
+    //Wait for promises to resolve. 
+    Promise.all([
+        Icon.getImageSource("md-map",30),
+        Icon.getImageSource("ios-share-alt",30)
+    ]).then(sources => {
+        Navigation.startTabBasedApp({
+            tabs: [
+                {
+                    screen: "awesome-places.FindPlaceScreen",
+                    label:  "Find Place",
+                    title: "Find Place",
+                    icon: sources[0]
+                },
+                {
+                    screen: "awesome-places.SharePlaceScreen",
+                    label:  "Share Place",
+                    title: "Share Place",
+                    icon: sources[1]
+                }
+            ]
+        });
     });
-};
+};    
+    
 
 export default startTabs;
 
