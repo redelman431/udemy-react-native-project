5a962f2469a61e92db770adfa1689368a9b164b2
Ross Edelman <ross@localhost.localdomain>
2018-06-19 16:04:48 -0400

    Preparation for animation complete.

--- rn-course/src/screens/FindPlace/FindPlace.js
+++ rn-course/src/screens/FindPlace/FindPlace.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 
 import PlaceList from '../../components/PlaceList/PlaceList';
@@ -8,6 +8,10 @@ class FindPlaceScreen extends Component {
 
     static navigatorStyle = {
         navBarButtonColor:"orange"
+    }
+
+    state = {
+        placesLoaded:false
     }
 
     constructor(props) {
@@ -25,6 +29,14 @@ class FindPlaceScreen extends Component {
 
         }
     }
+
+    placesSearchHandler = () => {
+        this.setState({
+            placesLoaded: true
+        })
+
+    }
+
     itemSelectedHandler = key => {
         const selPlace = this.props.places.find(place => {
             return place.key === key;
@@ -39,14 +51,47 @@ class FindPlaceScreen extends Component {
     }  
 
     render() {
+        let content = (
+            <TouchableOpacity onPress={this.placesSearchHandler}>
+                <View style = {styles.searchButton}>
+                    <Text style={styles.searchButtonText}>Find Places</Text>
+                </View>
+            </TouchableOpacity>
+        )
+        if(this.state.placesLoaded) {
+            content= (
+                        <PlaceList places={this.props.places} 
+                            onItemSelected={this.itemSelectedHandler}
+                         />
+            );
+        }
         return(
-            <View>
-                <PlaceList places={this.props.places} onItemSelected={this.itemSelectedHandler} />
+            <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+                {content}
             </View>
         );
     }
 
 }
+const styles = StyleSheet.create({
+    buttonContainer: {
+        flex: 1,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    searchButton: {
+        borderColor:"orange",
+        borderWidth: 3,
+        borderRadius: 50,
+        padding: 20
+
+    },
+    searchButtonText: {
+        color:"orange",
+        fontWeight:"bold",
+        fontSize: 26
+    }
+})
 
 const mapStateToProps = state => {
     return {
