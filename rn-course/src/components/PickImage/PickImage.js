b410a911d2a1706a0175834c036149602c5cec1b
Ross Edelman <ross@localhost.localdomain>
2018-06-25 11:18:08 -0400

    Image picker functionality added in SharePlace.

--- rn-course/src/components/PickImage/PickImage.js
+++ rn-course/src/components/PickImage/PickImage.js
@@ -1,16 +1,36 @@
 import React, {Component} from 'react'; 
 import {View, Image,Button, StyleSheet} from 'react-native';
-import imagePlaceholder from "../../assets/beautiful-place.jpg"
+import ImagePicker from "react-native-image-picker";
 
 class PickImage extends Component {
+    state = {
+        pickedImage: null
+    }
+
+    pickImageHandler = () => {
+        ImagePicker.showImagePicker({title:"Pick an Image"}, res => {
+            if(res.didCancel) {
+                console.log("User cancelled");
+            } else if(res.error) {
+                console.log("Error",res.error)
+            } else {
+                //Successful
+                this.setState({
+                    pickedImage: {uri: res.uri}
+                })
+            }
+
+        });
+    }
+
     render() {
         return( 
             <View style={styles.container}>
                 <View style={styles.placeholder}>
-                    <Image source ={imagePlaceholder} style={styles.previewImage} />
+                    <Image source ={this.state.pickedImage} style={styles.previewImage} />
                 </View>
                 <View style={styles.button}>
-                    <Button title="Pick Image" onPress={() => alert('Pick Image') }/>
+                    <Button title="Pick Image" onPress={this.pickImageHandler }/>
                 </View>
             </View>
         );
