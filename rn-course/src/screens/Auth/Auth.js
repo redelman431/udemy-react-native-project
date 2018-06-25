8063636fbee7db7800b3b660de50dfe8676d3135
Ross Edelman <ross@localhost.localdomain>
2018-06-25 10:41:34 -0400

    Auth screen security bug fixed.

--- rn-course/src/screens/Auth/Auth.js
+++ rn-course/src/screens/Auth/Auth.js
@@ -224,7 +224,7 @@ class AuthScreen extends Component {
             color="#29aaf4" 
             onPress={this.loginHandler}
             disabled = {  !this.state.controls.confirmPassword.valid && this.state.authMode === "signup" ||
-                          !this.state.controls.email ||
+                          !this.state.controls.email.valid ||
                           !this.state.controls.password.valid
                        }
           >
