39f7eb5e30ec9608ecc1c937d0a7d99d53888946
Ross Edelman <ross@localhost.localdomain>
2018-06-26 14:44:15 -0400

    Firebase API updates.

--- rn-course/src/store/actions/places.js
+++ rn-course/src/store/actions/places.js
@@ -1,11 +1,13 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
+import Secrets from '../../../secrets';
 export const addPlace = (placeName, location, image) => {
     return dispatch => {
         const placeData = {
             name:placeName,
             location: location
         };
-        fetch("https://awesome-places-1529610516638.firebaseio.com/places.json",{
+     
+        fetch("https://"+Secrets.FIREBASE_ID+".firebaseio.com/places.json",{
             method:"POST",
             body: JSON.stringify(placeData)
         })
