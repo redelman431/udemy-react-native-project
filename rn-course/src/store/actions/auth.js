e0c4772cea7b8b02a33ac7957c10e3830ef621fd
Ross Edelman <ross@localhost.localdomain>
2018-06-28 11:12:54 -0400

    Authentication functionality in firebase enabled.

--- rn-course/src/store/actions/auth.js
+++ rn-course/src/store/actions/auth.js
@@ -1,8 +1,36 @@
 import {TRY_AUTH} from './actionTypes';
+import Secrets from '../../../secrets';
 
 export const tryAuth = (authData) => {
-    return {
+    /*return {
         type: TRY_AUTH,
         authData: authData
+    }*/
+    return dispatch => {
+        dispatch(authSignup(authData));
+    };
+}
+
+export const authSignup = (authData) => {
+    return dispatch => {
+        fetch("https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key="+Secrets.FIREBASE_API_KEY, {
+            method: "POST",
+            body:JSON.stringify({
+                email: authData.email,
+                password: authData.password,
+                returnSecureToken: true
+            }),
+            headers: {
+               "Content-type": "application/json"
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            alert("Authentication failed, please try again!");
+        })
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        });
     }
 }
